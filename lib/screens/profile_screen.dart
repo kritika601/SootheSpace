7a4a5d6f12498fc36bd094d9674b91067125e46f
kritika601 <108182274+kritika601@users.noreply.github.com>
2023-05-15 16:44:35 +0530

    profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soothe_space/screens/main_home_screen.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -20,8 +21,50 @@ class _ProfileState extends State<Profile> {
             fontSize: 35,
           ),
         ),
+
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context)=> Home(),)
+            );
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            size:35.0,
+          ),
+        ),
+
         centerTitle: true,
+
       ),
+        body: Padding(
+        padding: const EdgeInsets.all(20.0),
+
+        child: Column(
+        children: <Widget>[
+        Container(
+        alignment: Alignment.topCenter,
+        child: Column(
+        children: [
+        Padding(
+        padding: const EdgeInsets.only(top:15.0),
+        child: Stack(
+        children: <Widget>[
+        Image.asset('assets/images/img30.png'),
+
+
+
+
+    ]
+    ),
+    ),
+    ]
+    ),
+        ),
+        ]
+    ),
+    ),
     );
   }
 }
